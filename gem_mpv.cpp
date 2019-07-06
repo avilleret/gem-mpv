aefa9b76dc7722b66991f65a445e27ab43625374
Antoine Villeret <antoine.villeret@gmail.com>
2019-07-06 19:36:29 +0200

    handle command and property setting & getting

--- gem_mpv.cpp
+++ gem_mpv.cpp
@@ -1,11 +1,26 @@
 #include "gem_mpv.hpp"
-#include "SDL2/SDL.h"
+#include <string.h>
+
+// greatly inspired by mvp-examples/libmpv/sdl
 
 CPPEXTERN_NEW_WITH_GIMME(mpv);
+
+static void wakeup(void *ctx)
+{
+  static_cast<mpv*>(ctx)->rise_event_flag();
+}
+
+static void *get_proc_address_mpv(void *fn_ctx, const char *name)
+{
+  // TODO adapt for Windows and MacOS
+  return (void *)glXGetProcAddress((const GLubyte*)name);
+}
 
 mpv::mpv(int argc, t_atom*argv)
   : gemframebuffer(argc, argv)
 {
+  m_prop_outlet = outlet_new(this->x_obj, 0);
+
   m_mpv = mpv_create();
   if (!m_mpv)
   {
@@ -15,7 +30,21 @@ mpv::mpv(int argc, t_atom*argv)
 
   // Some minor options can only be set before mpv_initialize().
   if (mpv_initialize(m_mpv) < 0)
-      error("mpv: init failed");
+    error("mpv: init failed");
+
+  // Actually using the opengl_cb state has to be explicitly requested.
+  // Otherwise, mpv will create a separate platform window.
+  if (mpv_set_option_string(m_mpv, "vo", "opengl-cb") < 0)
+  {
+    error("failed to set VO");
+    return;
+  }
+
+  mpv_observe_property(m_mpv, 0, "duration", MPV_FORMAT_DOUBLE);
+  mpv_observe_property(m_mpv, 0, "time-pos", MPV_FORMAT_DOUBLE);
+  mpv_observe_property(m_mpv, 0, "width",    MPV_FORMAT_INT64);
+  mpv_observe_property(m_mpv, 0, "height",   MPV_FORMAT_INT64);
+  mpv_set_wakeup_callback(m_mpv, wakeup, this);
 }
 
 mpv::~mpv()
@@ -28,29 +57,43 @@ void mpv::render(GemState *state)
 {
   gemframebuffer::render(state);
 
-  mpv_opengl_fbo fbo{(int)m_frameBufferIndex, m_width, m_height, m_format};
-  int flip(1);
+  while(m_mpv && m_event_flag)
+  {
+    mpv_event *event = mpv_wait_event(m_mpv, 0);
+    switch (event->event_id)
+    {
+      case MPV_EVENT_NONE:
+        m_event_flag=false;
+        break;
+      case MPV_EVENT_PROPERTY_CHANGE:
+      {
+        mpv_event_property *prop = (mpv_event_property *)event->data;
+        handle_prop_event(prop);
+        break;
+      }
+      default:
+        ;
+    }
+  }
 
-  mpv_render_param params[] = {
-      // Specify the default framebuffer (0) as target. This will
-      // render onto the entire screen. If you want to show the video
-      // in a smaller rectangle or apply fancy transformations, you'll
-      // need to render into a separate FBO and draw it manually.
-      {MPV_RENDER_PARAM_OPENGL_FBO, &fbo},
-      // Flip rendering (needed due to flipped GL coordinate system).
-      {MPV_RENDER_PARAM_FLIP_Y,  &flip},
-      {}
-  };
-  // See render_gl.h on what OpenGL environment mpv expects, and
-  // other API details.
+  if(m_size_changed)
+  {
+    t_atom atom[2];
+    SETFLOAT(atom, m_media_width);
+    SETFLOAT(atom+1, m_media_height);
+    outlet_anything(m_prop_outlet, gensym("resolution"), 2, atom);
+
+    if(m_auto_resize)
+    {
+      gemframebuffer::dimMess(m_media_width, m_media_height);
+    }
+    m_size_changed=false;
+  }
+
   if(m_mpv_gl)
-    mpv_render_context_render(m_mpv_gl, params);
-}
-
-static void *get_proc_address_mpv(void *fn_ctx, const char *name)
-{
-  // TODO adapt for Windows and MacOS
-  return (void *)glXGetProcAddress((const GLubyte*)name);
+  {
+    mpv_opengl_cb_draw(m_mpv_gl, m_frameBufferIndex, m_width, m_height);
+  }
 }
 
 void mpv::startRendering(void)
@@ -61,25 +104,27 @@ void mpv::startRendering(void)
   if(!m_mpv)
     return;
 
-  mpv_opengl_init_params gl_param{.get_proc_address = get_proc_address_mpv};
-
-  mpv_render_param params [] = {
-    {MPV_RENDER_PARAM_API_TYPE, (void*)MPV_RENDER_API_TYPE_OPENGL},
-    {MPV_RENDER_PARAM_OPENGL_INIT_PARAMS, &gl_param},
-    {MPV_RENDER_PARAM_INVALID, 0}
-  };
+  // The OpenGL API is somewhat separate from the normal mpv API. This only
+  // returns NULL if no OpenGL support is compiled.
+  m_mpv_gl = static_cast<mpv_opengl_cb_context*>(mpv_get_sub_api(m_mpv, MPV_SUB_API_OPENGL_CB));
+  if (!m_mpv_gl)
+  {
+    error("failed to create mpv GL API handle");
+    return;
+  }
 
   // This makes mpv use the currently set GL context. It will use the callback
-  // (passed via params) to resolve GL builtin functions, as well as extensions.
-  if (mpv_render_context_create(&m_mpv_gl, m_mpv, params) < 0)
+  // to resolve GL builtin functions, as well as extensions.
+  if (mpv_opengl_cb_init_gl(m_mpv_gl, NULL, get_proc_address_mpv, NULL) < 0)
   {
     error("failed to initialize mpv GL context");
     return;
   }
 
-  load_mess(m_file);
-
   m_init=true;
+  t_atom a;
+  SETSYMBOL(&a, m_file);
+  command_mess(gensym("load"), 1, &a);
 }
 
 void mpv::stopRendering(void)
@@ -88,35 +133,236 @@ void mpv::stopRendering(void)
   gemframebuffer::stopRendering();
 
   if(m_mpv_gl)
-    mpv_render_context_free(m_mpv_gl);
+    mpv_opengl_cb_uninit_gl(m_mpv_gl);
+
+  m_init=false;
 }
 
-void mpv::load_mess(const std::string& s)
+void mpv::command_mess(t_symbol *s, int argc, t_atom *argv)
 {
-  m_file = s;
+  std::string types{};
 
-  if(m_mpv && !m_file.empty())
+  if(s == gensym("command_typed") || s == gensym("property_typed"))
   {
-    const char *cmd[] = {"loadfile", m_file.c_str(), nullptr};
-    mpv_command(m_mpv, cmd);
+    if(argc < 2 || argv->a_type != A_SYMBOL || (argv+1)->a_type != A_SYMBOL)
+    {
+      error("usage : command_typed|property_typed <types> <command> <arguments...>");
+      return;
+    }
+    types = std::string(argv->a_w.w_symbol->s_name);
+    argv++;
+    argc--;
+  }
+
+  else if(argc == 0 || argv->a_type != A_SYMBOL )
+  {
+    error("usage: command|property <command> <arguments...>");
+    return;
+  }
+
+  if(!m_mpv)
+  {
+    error("mvp not initialized");
+    return;
+  }
+
+
+  if(s == gensym("command") || s == gensym("command_typed"))
+  {
+    if(!types.empty())
+      types = " " + types;
+    node_builder node(argc, argv, types);
+    mpv_node res;
+    if(mpv_command_node(m_mpv, node.node(), &res) < 0)
+    {
+      error("Error when executing command");
+      return;
+    }
+    node_autofree f(&res);
+  }
+  else if (s == gensym("property") || s == gensym("property_typed"))
+  {
+    auto sname = argv->a_w.w_symbol;
+    auto name = argv->a_w.w_symbol->s_name;
+    argv++;
+    argc--;
+
+    if(argc > 0)
+    {
+      node_builder node(argc, argv, types);
+      auto err = mpv_set_property(m_mpv, name, MPV_FORMAT_NODE, node.node());
+      if(err != MPV_ERROR_SUCCESS)
+      {
+        error("can't set property %s: error code: %d", name, err);
+      }
+    }
+    else
+    {
+      void* data{};
+      mpv_format format = MPV_FORMAT_DOUBLE;
+      if(!types.empty())
+      {
+        switch (types.at(0)) {
+          case 'b':
+            format = MPV_FORMAT_FLAG;
+            data = new bool();
+            break;
+          case 'd':
+            format = MPV_FORMAT_DOUBLE;
+            data = new double();
+            break;
+          case 'i':
+            format = MPV_FORMAT_INT64;
+            data = new int64_t();
+            break;
+          case 's':
+            format = MPV_FORMAT_STRING;
+            break;
+          default:
+            ;
+        }
+      }
+      auto err = mpv_get_property(m_mpv, name, format, data);
+      if(err != MPV_ERROR_SUCCESS)
+      {
+        error("can't get property %s, error code: %d", name, err);
+        return;
+      }
+
+      switch (format) {
+        case MPV_FORMAT_FLAG:
+        {
+          bool b = *(bool *)data;
+          t_atom a[2];
+          SETSYMBOL(a, sname);
+          SETFLOAT(a+1, b ? 1. : 0.);
+          outlet_anything(m_prop_outlet, gensym("property"), 2, a);
+          delete data;
+          break;
+        }
+        case MPV_FORMAT_DOUBLE:
+        {
+          double d = *(double *)data;
+          t_atom a[2];
+          SETSYMBOL(a, sname);
+          SETFLOAT(a+1, d);
+          outlet_anything(m_prop_outlet, gensym("property"), 2, a);
+          delete data;
+          break;
+        }
+        case MPV_FORMAT_INT64:
+        {
+          int64_t i = *(int64_t*)data;
+          t_atom a[2];
+          SETSYMBOL(a, sname);
+          SETFLOAT(a+1, i);
+          outlet_anything(m_prop_outlet, gensym("property"), 2, a);
+          delete data;
+          break;
+        }
+        case MPV_FORMAT_STRING:
+        {
+          char* s = (char *)data;
+          t_atom a[2];
+          SETSYMBOL(a, sname);
+          SETSYMBOL(a+1, gensym(s));
+          outlet_anything(m_prop_outlet, gensym("property"), 2, a);
+          break;
+        }
+        case MPV_FORMAT_NODE:
+        {
+          mpv_node* node = (mpv_node*)data;
+          switch(node->format)
+          {
+            case MPV_FORMAT_STRING:
+            {
+              t_atom a[2];
+              SETSYMBOL(a, sname);
+              SETSYMBOL(a+1, gensym(node->u.string));
+              outlet_anything(m_prop_outlet, gensym("property"), 2, a);
+              break;
+            }
+            case MPV_FORMAT_FLAG:
+            case MPV_FORMAT_INT64:
+            case MPV_FORMAT_DOUBLE:
+            {
+              t_atom a[2];
+              SETSYMBOL(a, sname);
+              switch (node->format) {
+                case MPV_FORMAT_FLAG:
+                  SETFLOAT(a+1, node->u.flag);
+                  break;
+                case MPV_FORMAT_INT64:
+                  SETFLOAT(a+1, node->u.int64);
+                  break;
+                case MPV_FORMAT_DOUBLE:
+                  SETFLOAT(a+1, node->u.double_);
+                  break;
+                default:
+                  return;
+              }
+              outlet_anything(m_prop_outlet, gensym("property"), 2, a);
+              break;
+            }
+            default:
+            {
+              error("could not handle this node format : %d", node->format);
+            }
+          }
+          mpv_free_node_contents(node);
+        }
+        case MPV_FORMAT_NODE_ARRAY:
+        case MPV_FORMAT_NODE_MAP:
+        {
+
+        }
+      }
+    }
+  }
+
+}
+
+void mpv::handle_prop_event(mpv_event_property *prop)
+{
+  if (strcmp(prop->name, "time-pos") == 0) {
+    if (prop->format == MPV_FORMAT_DOUBLE) {
+      double time = *(double *)prop->data;
+      t_atom atom;
+      SETFLOAT(&atom, time);
+      outlet_anything(m_prop_outlet, gensym("time-pos"), 1, &atom);
+    }
+  } else if (strcmp(prop->name, "duration") == 0) {
+    if (prop->format == MPV_FORMAT_DOUBLE) {
+      double time = *(double *)prop->data;
+      t_atom atom;
+      SETFLOAT(&atom, time);
+      outlet_anything(m_prop_outlet, gensym("duration"), 1, &atom);
+    }
+  } else if (strcmp(prop->name, "width") == 0) {
+    if (prop->format == MPV_FORMAT_INT64) {
+      int64_t val = *(int64_t *)prop->data;
+      m_media_width = val;
+      m_size_changed = true;
+    }
+  } else if (strcmp(prop->name, "height") == 0) {
+    if (prop->format == MPV_FORMAT_INT64) {
+      int64_t val = *(int64_t *)prop->data;
+      m_media_height = val;
+      m_size_changed = true;
+    }
   }
 }
 
-void mpv::pause_mess()
+void mpv :: dimen_mess(int width, int height)
 {
-  if(m_mpv)
+  if(width < 0 && height < 0)
   {
-    const char *cmd[] = {"pause", "1", nullptr};
-    mpv_command(m_mpv, cmd);
+    m_auto_resize = true;
+    gemframebuffer::dimMess(m_media_width, m_media_height);
   }
-}
-
-void mpv::play_mess()
-{
-  if(m_mpv)
+  else
   {
-    const char *cmd[] = {"play", nullptr};
-    mpv_command(m_mpv, cmd);
+    gemframebuffer::dimMess(width, height);
   }
 }
 
@@ -124,7 +370,10 @@ void mpv::obj_setupCallback(t_class *classPtr)
 {
   gemframebuffer::obj_setupCallback(classPtr);
 
-  CPPEXTERN_MSG1(classPtr, "load",   load_mess, std::string);
-  CPPEXTERN_MSG0(classPtr, "play",   play_mess);
-  CPPEXTERN_MSG0(classPtr, "pause",  pause_mess);
+  CPPEXTERN_MSG2(classPtr, "dimen",  dimen_mess, int, int); // override gemframebuffer method
+  CPPEXTERN_MSG(classPtr, "command",  command_mess);
+  CPPEXTERN_MSG(classPtr, "command_typed",  command_mess);
+  CPPEXTERN_MSG(classPtr, "property",  command_mess);
+  CPPEXTERN_MSG(classPtr, "property_typed",  command_mess);
+
 }
--- gem_mpv.hpp
+++ gem_mpv.hpp
@@ -3,29 +3,193 @@
 #include "gemframebuffer.h"
 
 #include <mpv/client.h>
-#include <mpv/render_gl.h>
+#include <mpv/opengl_cb.h>
+
+#include <atomic>
+#include <string.h>
+
+// some helper functions
+// from qthelper.hpp (see qt_opengl in mpv-examples)
+
+struct node_builder {
+    node_builder(int argc, t_atom* argv, std::string types = {}) {
+      m_types=types;
+      char type{};
+      if(!m_types.empty())
+        type = m_types.at(0);
+      set(&node_, argc, argv, type);
+    }
+    ~node_builder() {
+      free_node(&node_);
+    }
+    mpv_node *node() { return &node_; }
+  private:
+    // disable copy by making copy constructor
+    // and copy assignment operators private
+    node_builder(const node_builder&);
+    node_builder& operator=(const node_builder&);
+
+    mpv_node node_;
+    std::string m_types;
+    unsigned int m_index;
+
+    mpv_node_list *create_list(mpv_node *dst, bool is_map, int num) {
+      dst->format = is_map ? MPV_FORMAT_NODE_MAP : MPV_FORMAT_NODE_ARRAY;
+      mpv_node_list *list = new mpv_node_list();
+      dst->u.list = list;
+      if (!list)
+        goto err;
+      list->values = new mpv_node[num]();
+      if (!list->values)
+        goto err;
+      if (is_map) {
+        list->keys = new char*[num]();
+        if (!list->keys)
+          goto err;
+      }
+      return list;
+    err:
+      free_node(dst);
+      return NULL;
+    }
+    /*
+    char *dup_qstring(const QString &s) {
+        QByteArray b = s.toUtf8();
+        char *r = new char[b.size() + 1];
+        if (r)
+            std::memcpy(r, b.data(), b.size() + 1);
+        return r;
+    }
+    bool test_type(const QVariant &v, QMetaType::Type t) {
+        // The Qt docs say: "Although this function is declared as returning
+        // "QVariant::Type(obsolete), the return value should be interpreted
+        // as QMetaType::Type."
+        // So a cast really seems to be needed to avoid warnings (urgh).
+        return static_cast<int>(v.type()) == static_cast<int>(t);
+    }
+    */
+    void set(mpv_node *dst, int argc, t_atom* argv, char type = 0) {
+      if(argc == 1)
+      {
+        if (argv->a_type == A_SYMBOL) {
+          t_symbol* s = argv->a_w.w_symbol;
+          if(!s)
+            return;
+          dst->format = MPV_FORMAT_STRING;
+          dst->u.string = strdup(s->s_name);
+          if (!dst->u.string)
+            goto fail;
+        } else if (argv->a_type == A_FLOAT) {
+          float f = argv->a_w.w_float;
+          switch(type)
+          {
+            case 'b':
+              dst->format = MPV_FORMAT_FLAG;
+              dst->u.flag = f > 0;
+              break;
+            case 'i':
+              dst->format = MPV_FORMAT_INT64;
+              dst->u.int64 = f;
+              break;
+            case 'd':
+            default:
+              dst->format = MPV_FORMAT_DOUBLE;
+              dst->u.double_ = f;
+          }
+        }
+      }
+      else
+      {
+        mpv_node_list *list = create_list(dst, false, argc);
+        if (!list)
+          goto fail;
+        list->num = argc;
+        for (int n = 0; n < argc; n++)
+        {
+          char tag{};
+          if(m_types != std::string{})
+          {
+            if(n > m_types.size())
+            {
+              error("types size < argc");
+            }
+            else
+              tag = m_types.at(n);
+          }
+          set(&list->values[n], 1, argv+n, tag);
+        }
+      }
+      return;
+    fail:
+      error("can't process command");
+      dst->format = MPV_FORMAT_NONE;
+    }
+
+    void free_node(mpv_node *dst) {
+      switch (dst->format) {
+        case MPV_FORMAT_STRING:
+          delete[] dst->u.string;
+          break;
+        case MPV_FORMAT_NODE_ARRAY:
+        case MPV_FORMAT_NODE_MAP: {
+          mpv_node_list *list = dst->u.list;
+          if (list) {
+            for (int n = 0; n < list->num; n++) {
+              if (list->keys)
+                delete[] list->keys[n];
+              if (list->values)
+                free_node(&list->values[n]);
+            }
+            delete[] list->keys;
+            delete[] list->values;
+          }
+          delete list;
+          break;
+        }
+        default: ;
+      }
+      dst->format = MPV_FORMAT_NONE;
+    }
+};
+struct node_autofree {
+    mpv_node *ptr;
+    node_autofree(mpv_node *a_ptr) : ptr(a_ptr) {}
+    ~node_autofree() { mpv_free_node_contents(ptr); }
+};
 
 class GEM_EXTERN mpv : public gemframebuffer
 {
-  CPPEXTERN_HEADER(mpv, GemBase);
+    CPPEXTERN_HEADER(mpv, GemBase);
 
-public:
-  mpv(int argc, t_atom*argv);
-  ~mpv();
+  public:
+    mpv(int argc, t_atom*argv);
+    ~mpv();
 
-  virtual void startRendering(void);
-  virtual void stopRendering(void);
-  virtual void render(GemState *state);
+    virtual void startRendering(void);
+    virtual void stopRendering(void);
+    virtual void render(GemState *state);
 
-  void load_mess(const std::string& s);
-  void pause_mess();
-  void play_mess();
+    void command_mess(t_symbol* s, int argc, t_atom* argv);
+    void dimen_mess(int width, int height);
 
-private:
-  mpv_handle* m_mpv{};
-  mpv_render_context* m_mpv_gl{};
+    void inline rise_event_flag(){ m_event_flag = true; };
 
-  bool m_init{};
-  std::string m_file{};
-  bool m_pause{};
+  private:
+    void handle_prop_event(mpv_event_property* prop);
+
+    mpv_handle* m_mpv{};
+
+    mpv_opengl_cb_context* m_mpv_gl{};
+
+    bool m_init{};
+    t_symbol* m_file{};
+    bool m_pause{};
+    std::atomic_bool m_event_flag; // flag rised when new event arrived
+    bool m_size_changed{};
+    bool m_auto_resize{};
+
+    int m_media_width;
+    int m_media_height;
+
+    t_outlet* m_prop_outlet{};
 };
